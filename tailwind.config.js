fc22feae09ad8ee8c15beb483666974bc20bde58
Jonas Ebert <jonas_ebert@outlook.de>
2024-04-20 13:18:22 +0200

    Removed tests

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,6 @@
-import plugin from '@sveltejs/adapter-auto';
-
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
-
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        'primary': ['Poppins'],
-        'secondary': ['Montserrat']
-      }
-    },
-  },
-  plugins: [],
+module.exports = {
+    content: [
+        './src/**/*.{svelte,js}',
+    ],
 }
