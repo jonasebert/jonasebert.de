027bae88ab9cd09cf7e35a54cc4146fe2412b401
Jonas Ebert <jonas_ebert@outlook.de>
2024-04-20 20:32:57 +0200

    Changed prerenderer

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,4 +5,4 @@ export const load = (async () => {
 }) satisfies PageServerLoad;
 
 // This can be false if you're using a fallback (i.e. SPA mode)
-export const prerender = true;
+export const prerender = false;
