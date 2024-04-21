b7dd1f299640e2faf7f60000c255ccb7f0ae458e
Jonas Ebert <jonas_ebert@outlook.de>
2024-04-21 15:19:55 +0200

    About: Implementation Fixes #14

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,139 @@
+<script>
+
+	import { handleContextMenu } from "../store";
+
+</script>
+
 <body class="font-primary">
-	<div>
-		<h1>Jonas Ebert - Über mich</h1>
-		<h2>The future is now!</h2>
+	<div class="container">
+		<div>
+			<h1>Lebenslauf</h1>
+			<h2>Berufserfahrung</h2>
+			<div class="my-6 flex items-start">
+				<div class="flex justify-center items-center rounded-x1 w-16 h-16 shrink-0" style="background-color: #00dcfe;">
+					<img src="/logo_gingco.png" alt="Logo Gingco Systems" class="w-12 h-12" on:contextmenu={handleContextMenu} draggable="false">
+				</div>
+				<div class="ml-10 flex flex-col justify-between">
+					<h3>Systemadministrator</h3>
+					<p class="text-xl">Gingco Systems GmbH</p>
+					<p class="text-md">seit November 2022</p>
+					<div>
+						<ul class="list-disc">
+							<li>Betreuung und Entwicklung von Docker-basierten Netzwerk- und Serverinfrastruktur</li>
+							<li>Administrativer 2nd- und 3rd-Level Support (Remote)</li>
+							<li>Performance- und Securitymanagement sowie Analyse und Beseitigung von Störungen</li>
+							<li>Erstellung und Umsetzung von IT-Konzepten</li>
+							<li>Kommunikation mit Kunden, externen Dienstleistern und Lieferanten</li>
+						</ul>
+					</div>
+					<div class="flex flex-row flex-wrap">
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">PHP</div>
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">Linux</div>
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">Docker</div>
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">ISO 27001</div>
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">Microsoft Intune</div>
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">Datenbankverwaltung</div>
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">Kunden-Support</div>
+					</div>
+				</div>
+			</div>
+			<div class="my-6 flex items-start">
+				<div class="flex justify-center items-center rounded-x1 w-16 h-16 shrink-0" style="background-color: #fff;">
+					<img src="/logo_lba.png" alt="Logo Gingco Systems" class="w-12 h-12" on:contextmenu={handleContextMenu} draggable="false">
+				</div>
+				<div class="ml-10 flex flex-col justify-between">
+					<h3>Fachinformatiker Anwendungsbetreuung</h3>
+					<p class="text-xl">Luftfahrt-Bundesamt</p>
+					<p class="text-md">Oktober 2021 - November 2022</p>
+					<div>
+						<ul class="list-disc">
+							<li>Administration des Telekommunikationssystem</li>
+							<li>IT-Usersupport (Vor Ort und Remote)</li>
+						</ul>
+					</div>
+					<div class="flex flex-row flex-wrap">
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">Telekommunikation</div>
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">Telekommunikationssysteme</div>
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">Linux</div>
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">Windows</div>
+					</div>
+				</div>
+			</div>
+			<div class="my-6 flex items-start">
+				<div class="flex justify-center items-center rounded-x1 w-16 h-16 shrink-0" style="background-color: #fff;">
+					<img src="/logo_steinel.png" alt="Logo Gingco Systems" class="w-12 h-12" on:contextmenu={handleContextMenu} draggable="false">
+				</div>
+				<div class="ml-10 flex flex-col justify-between">
+					<h3>Junior Data Manager PIM</h3>
+					<p class="text-xl">STEINEL GmbH</p>
+					<p class="text-md">Oktober 2021 - November 2022</p>
+					<div>
+						<ul class="list-disc">
+							<li>Entwicklung von Automatisierung wie Vertriebsdatenblätter, Preislisten, Katalog, Printwerbemittel etc.</li>
+							<li>Entwicklung von Schnittstellen, wie z. B. die automatische Aktualisierung der Steinel Homepage durch das PIM-System</li>
+							<li>Dataquality weiter ausbauen</li>
+							<li>Enge Kooperation mit internen Abteilungen sowie externen Dienstleistern</li>
+							<li>Eruierung und Festlegung von Prozessen und Workflows</li>
+							<li>Koordination und Kontrolle der externen Dienstleister</li>
+							<li>Umsetzung neuer gesetzlicher Vorgaben und Verordnungen</li>
+						</ul>
+					</div>
+					<div class="flex flex-row flex-wrap">
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">PHP</div>
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">Scripting</div>
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">HTML</div>
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">Datenmanagement</div>
+					</div>
+				</div>
+			</div>
+
+			<h2>Ausbildung</h2>
+			<div class="my-6 flex items-start">
+				<div class="flex justify-center items-center rounded-x1 w-16 h-16 shrink-0" style="background-color: #00dcfe;">
+					<img src="/logo_gingco.png" alt="Logo Gingco Systems" class="w-12 h-12" on:contextmenu={handleContextMenu} draggable="false">
+				</div>
+				<div class="ml-10 flex flex-col justify-between">
+					<h3>Fachinformatiker in der Fachrichtung Systemintegration</h3>
+					<p class="text-xl">STEINEL GmbH</p>
+					<p class="text-md">August 2018 - Juni 2021</p>
+					<div>
+						<ul class="list-disc">
+							<li>IT-Usersupport (Vor Ort und Remote)</li>
+							<li>Support der Druckerumgebung (inbes. EU-Label Drucker)</li>
+							<li>Installation & Einrichtung von PCs/Mobilen Endgeräten</li>
+							<li>Erstellen & Aktualisieren der Windows 10 Images für die Installation der Geräte</li>
+							<li>Erstellen, koordinieren & halten von IT-Schulungen (Allgemeine IT Schulung & Informationssicherheitsschulung)</li>
+							<li>Erstellen einer Dokumentation der WLAN Umgebung am Standort Herzebrock</li>
+						</ul>
+					</div>
+					<div class="flex flex-row flex-wrap">
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">Windows</div>
+						<div class="text-sm rounded-lg bg-gray-400 py-1 px-1 mr-2 mb-1">Kunden-Support</div>
+					</div>
+				</div>
+			</div>
+
+			<h2>Ehrenamt</h2>
+			<div class="my-6 flex items-start">
+				<div class="flex justify-center items-center rounded-x1 w-16 h-16 shrink-0" style="background-color: #005538;">
+					<img src="/logo_gruene.svg" alt="Logo B'90/Die Grünen" class="w-12 h-12" on:contextmenu={handleContextMenu} draggable="false">
+				</div>
+				<div class="ml-10 flex flex-col justify-between">
+					<h3>Koordination AG Digitales</h3>
+					<p class="text-xl">Bündnis 90/Die Grünen Braunschweig</p>
+					<p class="text-md">seit November 2023</p>
+				</div>
+			</div>
+			<div class="my-6 flex items-start">
+				<div class="flex justify-center items-center rounded-x1 w-16 h-16 shrink-0" style="background-color: #005538;">
+					<img src="/logo_gruene.svg" alt="Logo B'90/Die Grünen" class="w-12 h-12" on:contextmenu={handleContextMenu} draggable="false">
+				</div>
+				<div class="ml-10 flex flex-col justify-between">
+					<h3>Koordination Aktionsgruppe</h3>
+					<p class="text-xl">Bündnis 90/Die Grünen Braunschweig</p>
+					<p class="text-md">seit November 2023</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </body>
--- src/routes/store.js
+++ src/routes/store.js
@@ -23,3 +23,8 @@ export const logo = '/logo/logo_500x100.png';
 export const logo_clear = '/logo/logo_500x100_clear.png';
 export const logo_small = '/logo/logo_500x500.png';
 export const logo_small_clear = '/logo/logo_500x500_clear.png';
+
+// Image Context Menu
+export function handleContextMenu(event) {
+    event.preventDefault();
+}
