dc865ccbef29d1f7fe22e5cf00b85c2493f8f9e1
Jonas Ebert <jonas_ebert@outlook.de>
2024-05-19 17:44:15 +0200

    Calendar: Added Date formatting

--- src/routes/calendar/+page.ts
+++ src/routes/calendar/+page.ts
@@ -4,11 +4,48 @@ export const load = async ({ fetch }) => {
     if (!response.ok) {
         throw new Error(`Fehler beim Laden der Termine: ${response.statusText}`);
     }
-    const events = await response.json();
+    const { events } = await response.json();
 
-    // console.log(await posts);
+    // Format Date
+    const foramttedEvents = events.map(event => {
+        return {
+            ...event,
+            start: formatDate(event.start),
+            end: formatDate(event.end),
+            startday: formatDate(event.start, `day`),
+            endday: formatDate(event.end, `day`),
+            startmonth: formatDate(event.start, `month`),
+            endmonth: formatDate(event.end, `month`),
+            startyear: formatDate(event.start, `year`),
+            endyear: formatDate(event.end, `year`),
+            starttime: formatDate(event.start, `time`),
+            endtime: formatDate(event.end, `time`),
+            startdate: formatDate(event.start, `date`),
+            enddate: formatDate(event.end, `date`),
+        };
+    });
 
     return {
-        events
+        events: foramttedEvents
     };
 };
+
+function formatDate(isoString, type) {
+    const months = ['Jan', 'Feb', 'März', 'Apr', 'Mai', 'Juni', 'Juli', 'Aug', 'Sept', 'Okt', 'Nov', 'Dez'];
+    const date = new Date(isoString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const monthIndex = date.getMonth();
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+
+    switch (type) {
+        case `day`: return `${day}`;
+        case `month`: return `${months[monthIndex]}`;
+        case `year`: return `${year}`;
+        case `time`: return `${hours}:${minutes}`;
+        case `date`: return `${day}. ${months[monthIndex]} ${year}`;
+
+        default: return `${day}. ${months[monthIndex]} ${year} - ${hours}:${minutes}`;
+    }
+}
