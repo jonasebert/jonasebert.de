c3b7a3efd1b782f9aaa46b867da03082f73c6873
Jonas Ebert <jonas_ebert@outlook.de>
2024-05-20 17:53:23 +0200

    Error: Added styled error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,7 @@
 	import { page } from '$app/stores';
 </script>
 
-<div>
-	<h1 class="font-poppins">{$page.status}</h1>
-	<h2 class="font-poppins">{$page.error.message}</h2>
+<div class="flex flex-col items-center justify-center min-h-screen text-gray-700 font-poppins">
+    <h1 class="text-6xl font-bold">{$page.status}</h1>
+    <h2 class="text-2xl mt-2">{$page.error.message}</h2>
 </div>
