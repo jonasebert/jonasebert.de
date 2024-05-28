3281e36714c05f75b531b2f8451bdf79220b97a1
Jonas Ebert <jonas_ebert@outlook.de>
2024-05-28 20:13:34 +0200

    [Calendar] Adjusted formatting

--- src/routes/api/calendar/+server.ts
+++ src/routes/api/calendar/+server.ts
@@ -1,11 +1,10 @@
-// src/routes/calendar.js
-import pkg_ical from 'node-ical'; // Importieren Sie die Parse-Funktion von node-ical
+import pkg_ical from 'node-ical';
 import pkg_rrule from 'rrule';
 import { json } from '@sveltejs/kit';
-import fetch from 'node-fetch'; // Stellen Sie sicher, dass node-fetch installiert ist
+import fetch from 'node-fetch';
 
 const { RRule, RRuleSet, rrulestr } = pkg_rrule;
-const {parseICS} = pkg_ical;
+const { parseICS } = pkg_ical;
 
 export async function GET({ query }) {
     const webCalUrl = process.env.JONAS_EBERT_WEBCAL_URL;
@@ -13,15 +12,12 @@ export async function GET({ query }) {
     const twoMonthsLater = new Date(now.getFullYear(), now.getMonth() + 2, now.getDate());
 
     try {
-        // Verwenden Sie fetch, um die iCal-Daten mit einem benutzerdefinierten User-Agent abzurufen
         const response = await fetch(webCalUrl, {
             headers: {
                 'User-Agent': 'Jonas Ebert/1.0'
             }
         });
         const text = await response.text();
-
-        // Verwenden Sie node-ical, um die iCal-Daten zu parsen
         const data = parseICS(text);
         let events = [];
 
