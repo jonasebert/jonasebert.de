79b0bb1e31120b916d0a21b6b08c66ab9dd06cc3
Jonas Ebert <jonas_ebert@outlook.de>
2024-06-18 19:06:06 +0200

    Implement blog posts with prismic Fixes #112

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -1,7 +1,11 @@
 import { base } from "$app/paths";
 
 export async function load({ params, fetch, data }) {
-  const res = await fetch(`${base}/api/blog?type=all`);
+  const res = await fetch(`${base}/api/blog?type=all`, {
+    headers: {
+      'X-Blog-Type': 'all'
+    }
+  });
   const posts = await res.json();
   return {
     posts: posts,
