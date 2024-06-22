34c3edf247b0d065e7f4c271511d258b0f27298a
Jonas Ebert <jonas_ebert@outlook.de>
2024-06-22 18:09:00 +0200

    [BLOG] Changed single page to API

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -5,11 +5,11 @@ import { name } from '$lib/store';
     import * as ph from '@prismicio/helpers';
 
 	export let data;
-    let { post } = data;
+    let { posts } = data;
 
 	function teaserImage() {
-		if (ph.asImageSrc(post.data.teaser_image[0].image)) {
-			return ph.asImageSrc(post.data.teaser_image[0].image);
+		if (ph.asImageSrc(posts.data.teaser_image[0].image)) {
+			return ph.asImageSrc(posts.data.teaser_image[0].image);
 		} else {
 			return '/home/teaser.webp';
 		}
@@ -17,8 +17,8 @@ import { name } from '$lib/store';
 </script>
 
 <svelte:head>
-	<title>{ph.asText(post.data.title)} - {name}</title>
-	<meta property="og:title" content={ph.asText(post.data.title)} />
+	<title>{ph.asText(posts.data.title)} - {name}</title>
+	<meta property="og:title" content={ph.asText(posts.data.title)} />
 	<meta property="og:image" content={teaserImage()} />
 </svelte:head>
 
@@ -26,7 +26,7 @@ import { name } from '$lib/store';
 	<div class="bg-black bg-opacity-50 p-5">
 		<article class="container mx-auto text-justify py-10">
 			<div class="text-white text-center font-poppins">
-				<h1 class="text-4xl font-bold text-white my-2">{ph.asText(post.data.title)}</h1>
+				<h1 class="text-4xl font-bold text-white my-2">{ph.asText(posts.data.title)}</h1>
 			</div>
 		</article>
 	</div>
@@ -34,27 +34,27 @@ import { name } from '$lib/store';
 
 <div class="container mx-auto p-5 text-pretty text-justify bg-je-gray-800">
 	<div class="flex flex-wrap justify-center items-center gap-2 font-montserrat">
-		{#if post.data.teaser_image[0].image.copyright}
+		{#if posts.data.teaser_image[0].image.copyright}
 			<p class="text-lg md:mr-40">
-				{#if post.data.teaser_image[0].copyright_link}
-					📸 <a href={post.data.teaser_image[0].copyright_link.url} target={post.data.teaser_image[0].copyright_link.target}>
-						{post.data.teaser_image[0].image.copyright}
+				{#if posts.data.teaser_image[0].copyright_link}
+					📸 <a href={posts.data.teaser_image[0].copyright_link.url} target={posts.data.teaser_image[0].copyright_link.target}>
+						{posts.data.teaser_image[0].image.copyright}
 					</a>
 				{:else}
-					📸 {post.data.teaser_image[0].image.copyright}
+					📸 {posts.data.teaser_image[0].image.copyright}
 				{/if}
 			</p>
 		{/if}
 		<p class="text-lg md:mr-40">
-            {#if post.data.overwrite_publish_date}
-                Veröffentlicht am: {FormatDate(ph.asDate(post.data.overwrite_publish_date),'day')}. {FormatDate(ph.asDate(post.data.overwrite_publish_date),'monthshort')} {FormatDate(ph.asDate(post.data.overwrite_publish_date),'year')}
+            {#if posts.data.overwrite_publish_date}
+                Veröffentlicht am: {FormatDate(ph.asDate(posts.data.overwrite_publish_date),'day')}. {FormatDate(ph.asDate(posts.data.overwrite_publish_date),'monthshort')} {FormatDate(ph.asDate(posts.data.overwrite_publish_date),'year')}
             {:else}
-                Veröffentlicht am: {FormatDate(ph.asDate(post.first_publication_date),'day')}. {FormatDate(ph.asDate(post.first_publication_date),'monthshort')} {FormatDate(ph.asDate(post.first_publication_date),'year')}
+                Veröffentlicht am: {FormatDate(ph.asDate(posts.first_publication_date),'day')}. {FormatDate(ph.asDate(posts.first_publication_date),'monthshort')} {FormatDate(ph.asDate(posts.first_publication_date),'year')}
             {/if}
         </p>
 		<div class="flex flex-wrap gap-2">
-			{#if post.tags[0]}
-				{#each post.tags as category}
+			{#if posts.tags[0]}
+				{#each posts.tags as category}
 					<div class="text-sm rounded-lg bg-green-400 text-je-gray-500 py-1 px-2">
 						<a href="/blog/category/{category}">
 							{category}
@@ -69,7 +69,7 @@ import { name } from '$lib/store';
 <div class="container mx-auto p-5 text-pretty md:text-pretty" style="font-family: Montserrat">
 	<div class="mt-8">
 		<article>
-			{#each post.data.body as contentBlock}
+			{#each posts.data.body as contentBlock}
 				{#if contentBlock.type === 'paragraph'}
 					<p>{contentBlock.text}</p>
 				{:else if contentBlock.type === 'heading1'}
--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,19 +1,24 @@
-import { base } from "$app/paths";
 import * as ph from '@prismicio/helpers';
 import { getDesc } from "$lib/util/TextHelpers.js";
 
-// @type {import('./$types').PageLoad}
-export async function load({ params, fetch, data }) {
-  const res = await fetch(`${base}/blog/${params.slug}.json`);
-  const post = await res.json();
-  return {
-    post: post.post,
-    title: ph.asText(post.post.data.title),
-    // description: getDesc(
-    //   ph.asText(post.post.data.body[0].primary.content),
-    //   250
-    // ),
-    keywords: post.post.tags.join(", "),
-    image: ph.asImageSrc(post.post.data.teaser_image),
-  };
+export async function load({ params, fetch }) {
+  const res = await fetch(`https://api.jonasebert.de/api?type=blog&itemtype=post&postid=${params.slug}`);
+  if (res.ok) {
+    const post = await res.json();
+    return {
+      posts: post.data,
+      title: ph.asText(post.data.title),
+      // description: getDesc(
+      //   ph.asText(post.data.body[0].primary.content),
+      //   250
+      // ),
+      keywords: post.data.tags.join(", "),
+      image: ph.asImageSrc(post.data.teaser_image)
+    };
+  } else {
+    // Error handling
+    return {
+      posts: []
+    };
+  }
 }
