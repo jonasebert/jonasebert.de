3b4ca23ec8eac3453f6ca9fa03f79246feb85127
Jonas Ebert <jonas.ebert@jonasebert.de>
2024-07-26 18:39:58 +0200

    [CALENDAR] Show InfoMessage if no events was found or an error at API-Fetch

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import InfoMessage from '$lib/components/blocks/InfoMessage.svelte';
     import CalendarTiles from '$lib/components/calendar_tiles.svelte';
     export let data;
 </script>
@@ -6,5 +7,11 @@
 <div class="container mx-auto px-4">
     <h1 class="text-5xl font-bold text-je-sand mt-2 pt-10 font-poppins">Termine</h1>
     <h2 class="text-4xl font-semibold text-je-sand mb-2 pb-10">Hier kannst du mich treffen!</h2>
-    <CalendarTiles items={data}></CalendarTiles>
+    {#if data.events[0]}
+        <CalendarTiles items={data}></CalendarTiles>
+    {:else}
+        <div class="text-center py-8 font-montserrat">
+            <InfoMessage message="Es konnten aktuell keine Veranstaltungen gefunden werden!"></InfoMessage>
+        </div>
+    {/if}
 </div>
