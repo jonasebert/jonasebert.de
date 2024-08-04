dd68b5c2fb902815b20f861bfe554d85e8817cc5
Jonas Ebert <jonas.ebert@jonasebert.de>
2024-08-04 17:53:13 +0200

    [CALENDAR] Added rendering for eventurl

--- src/lib/components/tiles/Calendar.svelte
+++ src/lib/components/tiles/Calendar.svelte
@@ -26,24 +26,40 @@
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
         {#each events as event}
         <div class={event.now == true ? 'bg-gray-700 rounded-lg overflow-hidden shadow-lg animate-pulse' : 'bg-gray-700 rounded-lg overflow-hidden shadow-lg'}>
-            <!-- <div class="transition-transform duration-500 hover:scale-105"> -->
-            <div>
-            <!-- <a href={event.summary}> -->
-            {#if event.teaserImage}
-                    <Image src={event.teaserImage} alt={`Teaser Bild ${event.summary}`} className="w-full"/>
+            {#if event.url}
+                <div class="transition-transform duration-500 hover:scale-105">
+                    <a href={event.url} target="_blank">
+                        {#if event.teaserImage}
+                            <Image src={event.teaserImage} alt={`Teaser Bild ${event.summary}`} className="w-full"/>
+                        {:else}
+                            <Image src="/home/teaser.webp" alt={`Teaser Bild ${event.summary}`} className="w-full"/>
+                        {/if}
+                    </a>
+                </div>
             {:else}
-                <Image src="/home/teaser.webp" alt={`Teaser Bild ${event.summary}`} className="w-full"/>
-            {/if}
-            <!-- </a> -->
-            </div>
+                <div>
+                    {#if event.teaserImage}
+                        <Image src={event.teaserImage} alt={`Teaser Bild ${event.summary}`} className="w-full"/>
+                    {:else}
+                        <Image src="/home/teaser.webp" alt={`Teaser Bild ${event.summary}`} className="w-full"/>
+                    {/if}
+                </div>
+            {/if}   
             <div class="p-4">
                 <h2 class="text-xl font-semibold mb-2 font-poppins text-je-sand">
-                    <!-- <a href={event.summary}> -->
-                    {#if event.state === 'CANCELLED'}
-                        [ABGESAGT]
+                    {#if event.url}
+                        <a href={event.url} target="_blank">
+                            {#if event.state === 'CANCELLED'}
+                                [ABGESAGT]
+                            {/if}
+                            {event.summary}
+                        </a>
+                    {:else}
+                        {#if event.state === 'CANCELLED'}
+                            [ABGESAGT]
+                        {/if}
+                        {event.summary}
                     {/if}
-                    {event.summary}
-                    <!-- </a> -->
                 </h2>
                 <div class="flex flex-row items-end gap-6 mb-2">
                     {#if event.datetype === 'date'}
