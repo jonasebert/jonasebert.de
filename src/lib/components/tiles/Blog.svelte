4610abef66774b63e2ca184883ed30ab759e732c
Jonas Ebert <jonas.ebert@jonasebert.de>
2024-08-16 09:36:38 +0200

    [BLOG] Fixed code formatting

--- src/lib/components/tiles/Blog.svelte
+++ src/lib/components/tiles/Blog.svelte
@@ -7,37 +7,37 @@
 
 <div class="bg-gray-700 rounded-lg overflow-hidden shadow-lg">
     <div class="transition-transform duration-500 hover:scale-105">
-    <a href={item.url}>
-    {#if item.data.teaser_image[0]}
-            <Image src={ph.asImageSrc(item.data.teaser_image[0].image)} alt="Teaser Blog {ph.asText(item.title)}" classNames="w-full"/>
-    {:else}
-        <Image src="/home/teaser.webp" alt="Teaser Blog {ph.asText(item.title)}" classNames="w-full"/>
-    {/if}
-    </a>
+        <a href={item.url}>
+        {#if item.data.teaser_image[0]}
+                <Image src={ph.asImageSrc(item.data.teaser_image[0].image)} alt="Teaser Blog {ph.asText(item.title)}" classNames="w-full"/>
+        {:else}
+            <Image src="/home/teaser.webp" alt="Teaser Blog {ph.asText(item.title)}" classNames="w-full"/>
+        {/if}
+        </a>
     </div>
     <div class="p-4">
-    <h2 class="text-xl font-semibold mb-2 font-poppins text-je-sand">
-        <a href={item.url}>
-            {ph.asText(item.data.title)}
-        </a>
-    </h2>
-    <p class="text-gray-300 text-sm mb-4 font-montserrat">
-        {#if item.data.overwrite_publish_date}
-            Veröffentlicht am {FormatDate(ph.asDate(item.data.overwrite_publish_date),'day')}. {FormatDate(ph.asDate(item.data.overwrite_publish_date),'monthshort')} {FormatDate(ph.asDate(item.data.overwrite_publish_date),'year')}
-        {:else}
-            Veröffentlicht am {FormatDate(ph.asDate(item.first_publication_date),'day')}. {FormatDate(ph.asDate(item.first_publication_date),'monthshort')} {FormatDate(ph.asDate(item.first_publication_date),'year')}
-        {/if}
-    </p>
-    <div class="flex flex-row flex-wrap gap-2 font-montserrat">
-        {#if item.tags[0]}
-        {#each item.tags as category}
-            <div class="text-sm rounded-lg bg-green-500 text-je-gray-500 py-1 px-2 mr-2 transition-transform duration-500 hover:scale-110">
-                <a href="/blog/category/{category}" target="_self" class="hover:text-je-gray-500">
-                    {category}
-                </a>
-            </div>
-        {/each}
-        {/if}
-    </div>
+        <h2 class="text-xl font-semibold mb-2 font-poppins text-je-sand">
+            <a href={item.url}>
+                {ph.asText(item.data.title)}
+            </a>
+        </h2>
+        <p class="text-gray-300 text-sm mb-4 font-montserrat">
+            {#if item.data.overwrite_publish_date}
+                Veröffentlicht am {FormatDate(ph.asDate(item.data.overwrite_publish_date),'day')}. {FormatDate(ph.asDate(item.data.overwrite_publish_date),'monthshort')} {FormatDate(ph.asDate(item.data.overwrite_publish_date),'year')}
+            {:else}
+                Veröffentlicht am {FormatDate(ph.asDate(item.first_publication_date),'day')}. {FormatDate(ph.asDate(item.first_publication_date),'monthshort')} {FormatDate(ph.asDate(item.first_publication_date),'year')}
+            {/if}
+        </p>
+        <div class="flex flex-row flex-wrap gap-2 font-montserrat">
+            {#if item.tags[0]}
+            {#each item.tags as category}
+                <div class="text-sm rounded-lg bg-green-500 text-je-gray-500 py-1 px-2 mr-2 transition-transform duration-500 hover:scale-110">
+                    <a href="/blog/category/{category}" target="_self" class="hover:text-je-gray-500">
+                        {category}
+                    </a>
+                </div>
+            {/each}
+            {/if}
+        </div>
     </div>
 </div>
