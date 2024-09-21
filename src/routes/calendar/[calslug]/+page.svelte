5c08fd016256ad454d2ba39f79b5ee67b262832a
Jonas Ebert <jonas_ebert@outlook.de>
2024-09-21 11:41:35 +0200

    [CALENDAR] New design structure for event detailed page

--- src/routes/calendar/[calslug]/+page.svelte
+++ src/routes/calendar/[calslug]/+page.svelte
@@ -23,54 +23,58 @@
 <div class="relative bg-fixed bg-no-repeat bg-center bg-cover" style="background-image: url({teaserImage().url});">
     <div class="bg-black bg-opacity-50 p-5">
         <div class="container mx-auto text-justify py-10">
-            <div class="text-white text-center font-poppins">
-                <h1 class="text-4xl font-bold my-2">{event.summary}</h1>
+            <div class="text-white text-center font-poppins mb-10">
+                <h1 class="text-4xl font-bold my-2">
+                    {#if event.state === 'CANCELLED'}
+                        [ABGESAGT]
+                    {/if}
+                    {event.summary}</h1>
             </div>
-        </div>
-    </div>
-</div>
-
-<div class="container mx-auto p-5 text-pretty text-justify bg-je-gray-800">
-    <div class="flex flex-wrap justify-center items-center gap-2 font-montserrat">
-        {#if teaserImage().copyright.url && teaserImage().copyright.text}
-            <p class="text-lg md:mr-40">
-                {#if teaserImage().copyright.text && teaserImage().copyright.url}
-                    📸 <a href={teaserImage().copyright.url} target="_blank">
-                        {teaserImage().copyright.text}
-                    </a>
-                {:else}
-                    📸 {teaserImage().copyright.text}
-                {/if}
-            </p>
-        {/if}
-        <div class="text-lg md:mr-40 flex flex-col">
-            {#if event.datetype === 'date'}
-                <p class="text-lg">
-                    {FormatDate(event.start, 'date')} - {FormatDate(event.end, 'date')}
-                </p>
-                <p class="text-lg">
-                    Ganztägig
-                </p>
-            {:else if FormatDate(event.start, 'date') === FormatDate(event.end, 'date') && event.datetype === 'date-time'}
-                <p class="text-lg">
-                    {FormatDate(event.start, 'datelong')}
-                </p>
-                <p class="text-lg">
-                    {FormatDate(event.start, 'time')} - {FormatDate(event.end, 'time')}
-                </p>
-            {:else}
-                <p class="text-lg">
-                    {FormatDate(event.start, 'date')} {FormatDate(event.start, 'time')} - {FormatDate(event.end, 'date')} {FormatDate(event.end, 'time')}
-                </p>
-            {/if}
-        </div>
-    </div>
-</div>
-
-<div class="container mx-auto p-5 text-pretty font-montserrat">
-    <div class="mt-8">
-        <div>
-            {event.description}
+            <div class="flex gap-5">
+                <div class="container mx-auto p-5 text-pretty font-montserrat">
+                    <div class="mt-8">
+                        <div>
+                            {event.description}
+                        </div>
+                    </div>
+                </div>
+                <div class="container mx-auto p-5 text-pretty text-justify rounded-lg  bg-je-green-500 bg-opacity-80">
+                    <div class="flex flex-col justify-center items-start gap-2 font-montserrat">
+                        {#if teaserImage().copyright.url && teaserImage().copyright.text}
+                            <p class="text-lg md:mr-40">
+                                {#if teaserImage().copyright.text && teaserImage().copyright.url}
+                                    📸 <a href={teaserImage().copyright.url} target="_blank">
+                                        {teaserImage().copyright.text}
+                                    </a>
+                                {:else}
+                                    📸 {teaserImage().copyright.text}
+                                {/if}
+                            </p>
+                        {/if}
+                        <div class="text-lg md:mr-40 flex flex-col">
+                            {#if event.datetype === 'date'}
+                                <p class="text-lg">
+                                    {FormatDate(event.start, 'date')} - {FormatDate(event.end, 'date')}
+                                </p>
+                                <p class="text-lg">
+                                    Ganztägig
+                                </p>
+                            {:else if FormatDate(event.start, 'date') === FormatDate(event.end, 'date') && event.datetype === 'date-time'}
+                                <p class="text-lg">
+                                    {FormatDate(event.start, 'datelong')}
+                                </p>
+                                <p class="text-lg">
+                                    {FormatDate(event.start, 'time')} - {FormatDate(event.end, 'time')}
+                                </p>
+                            {:else}
+                                <p class="text-lg">
+                                    {FormatDate(event.start, 'date')} {FormatDate(event.start, 'time')} - {FormatDate(event.end, 'date')} {FormatDate(event.end, 'time')}
+                                </p>
+                            {/if}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
