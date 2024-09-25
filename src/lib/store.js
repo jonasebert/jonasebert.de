7feab86d3f24ce78313568868835eabc58d488b0
Jonas Ebert <jonas.ebert@jonasebert.de>
2024-09-25 22:12:18 +0200

    [GLOBAL] Added troubleshooting for env

--- src/lib/store.js
+++ src/lib/store.js
@@ -22,8 +22,8 @@ export const contact = {
 export const uri = page;
 
 // API
-export const apiDomain = process.env.JONAS_EBERT_API_DOMAIN;
-export const apiSecret = process.env.JONAS_EBERT_API_SECRET;
+export const apiDomain = process.env.JONAS_EBERT_API_DOMAIN === '' ? 'api.jonasebert.de' : '';
+export const apiSecret = process.env.JONAS_EBERT_API_SECRET === '' ? '1234' : '';
 
 // Pictures
 // Logo
