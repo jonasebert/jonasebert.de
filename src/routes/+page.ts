e6b4b32b66f3656e44ed87f58fe99c6b5146c4d4
Jonas Ebert <jonas.ebert@jonasebert.de>
2024-09-25 19:22:17 +0200

    [GLOBAL] Troubleshooting API error

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,32 +4,34 @@ export async function load({ params, fetch }) {
 
   // Abrufen der Blog-Posts
   const postsRes = await fetch(`https://api.jonasebert.de?type=blog&itemtype=all&maxitems=5`, {
-  //   method: 'GET',
-    // headers: {
-    //     'Content-Type': 'application/json',
-    //     'x-vercel-protection-bypass': `${apiSecret}`
-    // }
-});
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'x-vercel-protection-bypass': `${apiSecret}`
+    }
+  });
   let posts = [];
   if (postsRes.ok) {
     const postsData = await postsRes.json();
     posts = postsData.data;
+    console.log(posts);
   } else {
     console.error('Fehler beim Abrufen der Posts:', postsRes.status, postsRes.statusText);
   }
 
   // Abrufen der Veranstaltungen
   const eventsRes = await fetch(`https://api.jonasebert.de?type=calendar&maxitems=5`, {
-    // method: 'GET',
-    // headers: {
-    //     'Content-Type': 'application/json',
-    //     'x-vercel-protection-bypass': `${apiSecret}`
-    // }
-});
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'x-vercel-protection-bypass': `${apiSecret}`
+    }
+  });
   let events = [];
   if (eventsRes.ok) {
     const eventsData = await eventsRes.json();
     events = eventsData.data;
+    console.log(events);
   } else {
     console.error('Fehler beim Abrufen der Veranstaltungen:', eventsRes.status, eventsRes.statusText);
   }
