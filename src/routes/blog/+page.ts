087e100318fa23a8f225a33a707b6a8e87e234c5
Jonas Ebert <jonas.ebert@jonasebert.de>
2024-09-30 15:47:43 +0200

    [BLOG] Removed MaxItems for API Call

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -5,7 +5,7 @@ export async function load({ params, fetch }) {
   let posts = [];
 
   try {
-    const postsRes = await fetch(`https://${apiDomain}/api?type=blog&itemtype=all&maxitems=5`);
+    const postsRes = await fetch(`https://${apiDomain}/api?type=blog&itemtype=all`);
 
     if (postsRes.ok) {
       const postsData = await postsRes.json();
