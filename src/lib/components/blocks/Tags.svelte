8d239cb6236a3aa4117f75373f75b8d5e94be190
Jonas Ebert <jonas.ebert@jonasebert.de>
2024-10-06 15:51:12 +0200

    Refactor Tags component to add support for clickable tags with links

--- src/lib/components/blocks/Tags.svelte
+++ src/lib/components/blocks/Tags.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
     export let text: string = '';
+    export let link: string = '';
+    export let clickable: boolean = false;
 </script>
 
-<div class="text-sm rounded-lg bg-green-500 text-je-gray-500 py-1 px-2 mr-2">{text}</div>
+{#if (clickable && link)}
+    <a class="text-sm rounded-lg bg-green-500 text-je-gray-500 py-1 px-2 mr-2 hover:text-je-gray-500 hover:no-underline transition-transform duration-500 hover:scale-105" href={link} target="_blank">
+        <div>{text}</div>
+    </a>
+{:else}
+    <div class="text-sm rounded-lg bg-green-500 text-je-gray-500 py-1 px-2 mr-2">{text}</div>
+{/if}
