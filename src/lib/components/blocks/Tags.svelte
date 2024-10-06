e5e642199dd78dc18d5cbee291364b9041aea3db
Jonas Ebert <jonas.ebert@jonasebert.de>
2024-10-06 16:12:22 +0200

    Refactor Tags component to support dynamic scaling and clickable links with target attribute

--- src/lib/components/blocks/Tags.svelte
+++ src/lib/components/blocks/Tags.svelte
@@ -3,10 +3,11 @@
     export let link: string = '';
     export let clickable: boolean = false;
     export let resize: number = 105;
+    export let target: string = '_blank';
 </script>
 
 {#if (clickable && link)}
-    <a class="text-sm rounded-lg bg-green-500 text-je-gray-500 py-1 px-2 mr-2 hover:text-je-gray-500 hover:no-underline transition-transform duration-500 hover:scale-{resize}" href={link} target="_blank">
+    <a class="text-sm rounded-lg bg-green-500 text-je-gray-500 py-1 px-2 mr-2 hover:text-je-gray-500 hover:no-underline transition-transform duration-500 hover:scale-{resize}" href={link} target={target}>
         <div>{text}</div>
     </a>
 {:else}
