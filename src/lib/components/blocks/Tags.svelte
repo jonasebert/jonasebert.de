6ba30ccdae5e0c722a316698b6e58310cb66172b
Jonas Ebert <jonas.ebert@jonasebert.de>
2024-10-06 15:56:40 +0200

    Refactor Tags component to add support for clickable tags with dynamic scaling

--- src/lib/components/blocks/Tags.svelte
+++ src/lib/components/blocks/Tags.svelte
@@ -2,10 +2,11 @@
     export let text: string = '';
     export let link: string = '';
     export let clickable: boolean = false;
+    export let resize: number = 105;
 </script>
 
 {#if (clickable && link)}
-    <a class="text-sm rounded-lg bg-green-500 text-je-gray-500 py-1 px-2 mr-2 hover:text-je-gray-500 hover:no-underline transition-transform duration-500 hover:scale-105" href={link} target="_blank">
+    <a class="text-sm rounded-lg bg-green-500 text-je-gray-500 py-1 px-2 mr-2 hover:text-je-gray-500 hover:no-underline transition-transform duration-500 hover:scale-{resize}" href={link} target="_blank">
         <div>{text}</div>
     </a>
 {:else}
