9892931418ec153db6c32f3b41fe87a3ef9d0bc5
Jonas Ebert <jonas.ebert@jonasebert.de>
2024-11-01 19:11:45 +0100

    Entferne Bluesky-Icon aus der Social-Komponente für aktualisierte soziale Links

--- src/lib/components/blocks/Social.svelte
+++ src/lib/components/blocks/Social.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import RoundedIcon from "$lib/components/button/RoundedIcon.svelte"
-    import { faInstagram, faThreads, faBluesky, faYoutube, faTiktok, faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons"; // For new Icons: https://fontawesome.com/search
+    import { faInstagram, faThreads, faYoutube, faTiktok, faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons"; // For new Icons: https://fontawesome.com/search
 
     export let hoverBGColor = "";
 </script>
@@ -8,7 +8,6 @@
 <div class="space-y-2">
     <RoundedIcon href="https://www.instagram.com/jonas_ebert" icon={faInstagram} target="_blank" hoverBGColor={hoverBGColor} />
     <RoundedIcon href="https://www.threads.net/@jonas_ebert" icon={faThreads} target="_blank" hoverBGColor={hoverBGColor} />
-    <RoundedIcon href="https://bsky.app/profile/jonasebert.bsky.social" icon={faBluesky} target="_blank" hoverBGColor={hoverBGColor} />
     <RoundedIcon href="https://www.youtube.com/@jonas_ebert" icon={faYoutube} target="_blank" hoverBGColor={hoverBGColor} />
     <RoundedIcon href="https://www.tiktok.com/@jonas.ebert3" icon={faTiktok} target="_blank" hoverBGColor={hoverBGColor} />
     <RoundedIcon href="https://www.linkedin.com/in/jonasebert" icon={faLinkedinIn} target="_blank" hoverBGColor={hoverBGColor} />
