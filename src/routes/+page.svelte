bda608872a03eedb4516fd4f6978301ea91d7b07
Jonas Ebert <jonas.ebert@jonasebert.de>
2024-11-24 14:27:05 +0100

    Update text colors to use 'je-magical-fata_morgana' for improved visual consistency

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,7 @@
 			<div class="p-8 font-poppins flex-1 justify-start md:justify-start lg:justify-center flex flex-col text-left lg:text-left relative overflow-hidden">
 				<div class="size-fit md:size-80">
 					<h2 class="text-2xl font-semibold text-je-luminous-nebellicht">The future is now!</h2>
-					<h1 class="text-5xl font-bold text-je-sand mt-2 mb-2">{ name }</h1>
+					<h1 class="text-5xl font-bold text-je-magical-fata_morgana mt-2 mb-2">{ name }</h1>
 					<div class="pb-8">
 						<Social hoverBGColor="je-mystical-waldtiefe-700" />
 					</div>
@@ -40,8 +40,8 @@
 <div class="container relative bg-[url('/home/teaser.webp')] bg-fixed bg-no-repeat bg-center bg-cover">
 	<div class="absolute inset-0 bg-black opacity-55"></div>
 	<div class="relative py-7 text-center text-balance">
-		<h2 class="text-4xl font-bold pb-7 uppercase text-je-sand font-poppins">Politik für eine lebenswerte Welt</h2>
-		<div class="mb-4 text-je-sand text-lg font-montserrat">
+		<h2 class="text-4xl font-bold pb-7 uppercase text-je-magical-fata_morgana font-poppins">Politik für eine lebenswerte Welt</h2>
+		<div class="mb-4 text-je-magical-fata_morgana text-lg font-montserrat">
 			Unser Klima verändert sich rasant und mit jedem Kilogramm CO₂ mehr wird dieser Planet zu einem lebensfeindlichen Ort. Wir steuern bereits auf eine Klimakatastrophe zu, die bald unvermeidlich sein wird. Wir stellen fest, dass unsere Sommer immer wärmer und unsere Winter immer milder werden. Wir feiern jedes Jahr einen neuen Temperaturrekord. Ein Umdenken in der Politik und Wirtschaft geht nur sehr langsam voran.
 		</div>
 		<div class="mb-4 text-je-sand text-lg font-montserrat">
@@ -50,7 +50,7 @@
 		<div class="mb-4 text-je-sand text-lg font-montserrat">
 			Es muss schnellstmöglich ein Umdenken in der Politik stattfinden und es müssen wirksame Lösungen in Richtung einer klimagerechten Welt erarbeitet werden, die alle Menschen einbezieht und die Lebensgrundlagen bewahrt. Eine Ausbeutung unseres Planeten darf nicht fortgesetzt werden!
 		</div>
-		<div class="mb-4 text-je-sand text-lg font-montserrat">
+		<div class="mb-4 text-je-magical-fata_morgana text-lg font-montserrat">
 			Mit diesen Worten stehe ich für eine besser und klimagerechte Welt!
 		</div>
 	</div>
@@ -58,8 +58,8 @@
 
 <div class="container mx-auto px-4 py-12">
 	<div class="text-center font-poppins">
-    	<h2 class="text-5xl font-semibold text-je-sand my-2">Termine</h2>
-    	<h3 class="text-4xl font-semibold text-je-sand my-2 pb-10">Hier kannst du mich treffen!</h3>
+    	<h2 class="text-5xl font-semibold text-je-magical-fata_morgana my-2">Termine</h2>
+    	<h3 class="text-4xl font-semibold text-je-magical-fata_morgana my-2 pb-10">Hier kannst du mich treffen!</h3>
 	</div>
     {#if events[0]}
         <CalendarTiles items={events}></CalendarTiles>
@@ -73,14 +73,14 @@
 <div class="container relative bg-[url('/home/brunswiek.webp')] bg-fixed bg-no-repeat bg-center bg-cover">
 	<div class="absolute inset-0 bg-black opacity-55"></div>
 	<div class="relative py-7 text-center text-balance">
-		<h2 class="text-4xl font-bold pb-7 uppercase text-je-sand font-poppins">Politik in Braunschweig</h2>
-		<div class="mb-4 text-je-sand text-lg font-montserrat">
+		<h2 class="text-4xl font-bold pb-7 uppercase text-je-magical-fata_morgana font-poppins">Politik in Braunschweig</h2>
+		<div class="mb-4 text-je-magical-fata_morgana text-lg font-montserrat">
 			Im Jahr 2021 zog ich in einer schwierigen Zeit nach Braunschweig. Ich war sehr aufgeregt, denn ich hatte gehört, dass diese Stadt eine ganz besondere Atmosphäre hat. Doch das schöne Braunschweig mit seinen historischen Sehenswürdigkeiten wie dem beeindruckenden Braunschweiger Dom und dem malerischen Magniviertel hat mich von den ersten Tagen an mit offenen Armen empfangen und begeistert.
 		</div>
-		<div class="mb-4 text-je-sand text-lg font-montserrat">
+		<div class="mb-4 text-je-magical-fata_morgana text-lg font-montserrat">
 			Im Jahr 2022 war ich sehr glücklich, Teil der Grünen Partei zu werden und meine politischen Interessen zu vertiefen und mich für meine Überzeugungen zu engagieren. Die Arbeit mit den Grünen hat mir die wunderbare Möglichkeit gegeben, aktiv an der Gestaltung unserer Stadt mitzuwirken und mich für Projekte einzusetzen, die mir sehr am Herzen liegen. Im April 2023 habe ich dann die Letzte Generation kennengelernt und durfte dort eine Weile mitwirken. Das hat mir ganz neue Perspektiven eröffnet, auch was den Klimaschutz und den zivilen Ungehorsam betrifft.
 		</div>
-		<div class="mb-4 text-je-sand text-lg font-montserrat">
+		<div class="mb-4 text-je-magical-fata_morgana text-lg font-montserrat">
 			Diese Erfahrungen haben mich dazu inspiriert, mich noch intensiver für Braunschweig einzusetzen. Es ist mir eine große Freude, derzeit die AG Digitales zu koordinieren und mich so leidenschaftlich für unser schönes Braunschweig einzusetzen. Mein Ziel ist es, ein buntes, offenes und lebendiges Braunschweig zu fördern und zu unterstützen. Die Vielfalt und Geschichte dieser Stadt, von den mittelalterlichen Weichbilden bis hin zur modernen Architektur des Happy RIZZI House, inspiriert mich täglich.
 		</div>
 	</div>
@@ -88,7 +88,7 @@
 
 <div class="container mx-auto px-4 py-12">
     <div class="text-center font-poppins">
-		  <h2 class="text-5xl font-semibold text-je-sand my-2 pb-7">Aktuelles</h2>
+		  <h2 class="text-5xl font-semibold text-je-magical-fata_morgana my-2 pb-7">Aktuelles</h2>
 	</div>
 	{#if posts[0]}
 		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 place-items-baseline">
