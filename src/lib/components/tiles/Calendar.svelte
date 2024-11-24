ee7f79e0124db726891806408f57a6fa38ee9746
Jonas Ebert <jonas.ebert@jonasebert.de>
2024-11-24 14:58:59 +0100

    Update Calendar component layout to align items to the end for improved visual consistency

--- src/lib/components/tiles/Calendar.svelte
+++ src/lib/components/tiles/Calendar.svelte
@@ -24,7 +24,7 @@
 </script>
 
 {#if events}
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 place-items-baseline">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 place-items-end">
         {#each events as event}
         <div class={event.now == true ? 'rounded-lg overflow-hidden shadow-lg animate-pulse' : 'rounded-lg overflow-hidden shadow-lg'}>
             <div class="relative z-10 transition-transform duration-500 hover:scale-105">
